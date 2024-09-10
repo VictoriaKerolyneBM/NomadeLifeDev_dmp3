460ed6e2a28c041b470c97bcf77924f2d3d3b1b1
VictoriaKerolyneBM <victoria.kbmedeiros@gmail.com>
2024-09-10 11:57:29 -0300

    Aula0910 - Footer e Navbar

---
+++ nomadelife-front/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './Footer.module.css'
+
+export const Footer = () => {
+  return (
+    <>
+    <footer className = {styles.footer}>
+        <h3>Plataforma para desenvolvedores que tem o espirito livre</h3>
+        <p>Nomade Developers</p>
+
+    </footer>
+    </>
+    
+
+  )
+}
